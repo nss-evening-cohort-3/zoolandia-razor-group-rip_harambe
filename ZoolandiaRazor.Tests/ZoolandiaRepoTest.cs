8b85a4505a4f22bc5bc3527f88873af5d9368ce5
David Cramb <david.cramb@gmail.com>
2016-10-08 14:44:42 -0500

    current unit tests and axed vs folder, new gitignore file

--- ZoolandiaRazor.Tests/ZoolandiaRepoTest.cs
+++ ZoolandiaRazor.Tests/ZoolandiaRepoTest.cs
@@ -13,21 +13,85 @@ namespace ZoolandiaRazor.Tests
     [TestClass]
     public class ZoolandiaRepoTest
     {
+        Mock<ZoolandiaRazorContext> mock_context { get; set; }
+        Mock<DbSet<Animals>> mock_animal_table { get; set; }
+        List<Animals> animal_list { get; set; }
+        ZoolandiaRazorRepo repo { get; set; }
+
+        public void ConnectMocksToDatastore()
+        {
+            var queryable_animal_list = animal_list.AsQueryable();
+            mock_animal_table.As<IQueryable<Animals>>().Setup(m => m.Provider).Returns(queryable_animal_list.Provider);
+            mock_animal_table.As<IQueryable<Animals>>().Setup(m => m.Expression).Returns(queryable_animal_list.Expression);
+            mock_animal_table.As<IQueryable<Animals>>().Setup(m => m.ElementType).Returns(queryable_animal_list.ElementType);
+            mock_animal_table.As<IQueryable<Animals>>().Setup(m => m.GetEnumerator()).Returns(() => queryable_animal_list.GetEnumerator());
+
+            mock_context.Setup(c => c.Animals).Returns(mock_animal_table.Object);
+            mock_animal_table.Setup(t => t.Add(It.IsAny<Animals>())).Callback((Animals a) => animal_list.Add(a));
+        }
+        [TestInitialize]
+        public void Initialize()
+        {
+            mock_context = new Mock<ZoolandiaRazorContext>();
+            mock_animal_table = new Mock<DbSet<Animals>>();
+            animal_list = new List<Animals>();
+            repo = new ZoolandiaRazorRepo(mock_context.Object);
+            ConnectMocksToDatastore();
+        }
+        [TestCleanup]
+        public void TearDown()
+        {
+            repo = null;
+        }
+
         [TestMethod]
         public void EnsureCanCreateInstanceOfModelClasses()
         {
-            Animals Animals = new Animals();
-            Employee Employees = new Employee();
-            HabitatType HabitatTypes = new HabitatType();
-            Exhibit Exhibits = new Exhibit();
-            Species Species = new Species();
             ZoolandiaRazorRepo Repo = new ZoolandiaRazorRepo();
-            Assert.IsNotNull(Animals);
-            Assert.IsNotNull(Employees);
-            Assert.IsNotNull(HabitatTypes);
-            Assert.IsNotNull(Exhibits);
-            Assert.IsNotNull(Species);
             Assert.IsNotNull(Repo);
+        }
+        [TestMethod]
+        public void EnsureCanCreateInstanceWithMockInitialized()
+        {
+            Assert.IsNotNull(repo);
+        }
+        [TestMethod]
+        public void EnsureRepoHasContext()
+        {
+            ZoolandiaRazorContext actual_context = repo.Context;
+            Assert.IsInstanceOfType(actual_context, typeof(ZoolandiaRazorContext));
+        }
+        [TestMethod]
+        public void EnsureAnimalDatabaseIsEmpty()
+        {
+            List<Animals> all_animals = repo.GetAllAnimals();
+            int expected_animal_count = 0;
+            int actual_animal_count = all_animals.Count();
+            Assert.AreEqual(expected_animal_count, actual_animal_count);
+        }
+        [TestMethod]
+        public void EnsureCanAddAnimalToDatabase()
+        {
+            Animals my_animal = new Animals { Name = "Bob", Age = 35, Exhibit = null, Species = null };
+            repo.AddNewAnimal(my_animal);
+            List<Animals> all_animals = repo.GetAllAnimals();
+            int expected_animal_count = 1;
+            int actual_animal_count = all_animals.Count();
+            Assert.AreEqual(expected_animal_count, actual_animal_count);
+        }
+        [TestMethod]
+        public void EnsureCanAddAnimalWithDifferentArgumentstoDatabase()
+        {
+            repo.AddNewAnimal("Steve", 20);
+            List<Animals> all_animals = repo.GetAllAnimals();
+            int expected_count = 1;
+            int actual_count = all_animals.Count();
+            Assert.AreEqual(expected_count, actual_count);
+        }
+        [TestMethod]
+        public void EnsureCanAddMultipleAnimalsToDatabase()
+        {
+
         }
     }
 }
--- ZoolandiaRazor/DAL/ZoolandiaRazorRepo.cs
+++ ZoolandiaRazor/DAL/ZoolandiaRazorRepo.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ZoolandiaRazor.Models;
 
 namespace ZoolandiaRazor.DAL
 {
@@ -16,6 +17,29 @@ namespace ZoolandiaRazor.DAL
         public ZoolandiaRazorRepo(ZoolandiaRazorContext _context)
         {
             Context = _context;
+        }
+
+        public List<Animals> GetAllAnimals()
+        {
+            return Context.Animals.ToList();
+        }
+        
+        //public void AddNewAnimal(string Name, int Age, Exhibit Exhibit, Species Species)
+        //{
+        //    Animals animal = new Animals {Name=Name, Age=Age, Exhibit=Exhibit, Species=Species };
+        //    Context.Animals.Add(animal);
+        //    Context.SaveChanges();
+        //}
+        public void AddNewAnimal(Animals my_animal)
+        {
+            Context.Animals.Add(my_animal);
+            Context.SaveChanges();
+        }
+        public void AddNewAnimal(string name, int age)
+        {
+            Animals animal = new Models.Animals { Name = name, Age = age };
+            Context.Animals.Add(animal);
+            Context.SaveChanges();
         }
     }
 }
