346c3edccf1a36a8d1ac578be7c0640aa8f02a13
David Cramb <david.cramb@gmail.com>
2016-10-08 10:53:42 -0500

    Added Context file

--- ZoolandiaRazor/DAL/ZoolandiaRazorContext.cs
+++ ZoolandiaRazor/DAL/ZoolandiaRazorContext.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using System.Data.Entity;
+using ZoolandiaRazor.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace ZoolandiaRazor.DAL
 {
-    public class ZoolandiaRazorContext
+    public class ZoolandiaRazorContext : DbContext
     {
+        public virtual DbSet<Animals> Animals { get; set; }
+        public virtual DbSet<Species> Species { get; set; }
+        public virtual DbSet<Employee> Employee { get; set; }
+        public virtual DbSet<HabitatType> Habitat { get; set; }
+        public virtual DbSet<Exhibit> Exhibit { get; set; }
+
     }
 }
