4aeab58498d141a836ae01623e9a50cea7f1e2e1
David Cramb <david.cramb@gmail.com>
2016-10-08 10:47:52 -0500

    Added models for Employee, Animal, Species, Habitat, etc.

---
+++ ZoolandiaRazor/Models/Animals.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ZoolandiaRazor.Models
+{
+    public class Animals
+    {
+        [Key]
+        public int AnimalId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public Exhibit Exhibit { get; set; }
+        [Required]
+        public Species Species { get; set; }
+    }
+}
