cc52afe16da81c47da31815d8237427a0cd89c62
David Cramb <david.cramb@gmail.com>
2016-10-09 19:04:39 -0500

    added crd methods for employees and unit tests

--- ZoolandiaRazor/DAL/ZoolandiaRazorRepo.cs
+++ ZoolandiaRazor/DAL/ZoolandiaRazorRepo.cs
@@ -23,6 +23,22 @@ namespace ZoolandiaRazor.DAL
         {
             return Context.Animals.ToList();
         }
+        public List<Employee> GetAllEmployees()
+        {
+            return Context.Employee.ToList();
+        }
+        public List<HabitatType> GetAllHabitats()
+        {
+            return Context.Habitat.ToList();
+        }
+        public List<Species> GetAllSpecies()
+        {
+            return Context.Species.ToList();
+        }
+        public List<Exhibit> GetAllExhibits()
+        {
+            return Context.Exhibit.ToList();
+        }
         
         //public void AddNewAnimal(string Name, int Age, Exhibit Exhibit, Species Species)
         //{
@@ -57,6 +73,103 @@ namespace ZoolandiaRazor.DAL
             }
             return animal_to_remove;
         }
+
+
+        public void AddNewEmployee(Employee my_employee)
+        {
+            Context.Employee.Add(my_employee);
+            Context.SaveChanges();
+        }
+        public Employee FindEmployeeByFirstName(string firstname)
+        {
+            Employee found_employee = Context.Employee.FirstOrDefault(e => e.FirstName.ToLower() == firstname.ToLower());
+            return found_employee;
+        }
+        public Employee FindEmployeeByLastName(string lastname)
+        {
+            Employee found_employee = Context.Employee.FirstOrDefault(e => e.LastName.ToLower() == lastname.ToLower());
+            return found_employee;
+        }
+        public Employee RemoveEmployeeByLastName(string lastName)
+        {
+            Employee employee_to_remove = FindEmployeeByLastName(lastName);
+            if (employee_to_remove != null)
+            {
+                Context.Employee.Remove(employee_to_remove);
+                Context.SaveChanges();
+            }
+            return employee_to_remove;
+        }
+        public Employee RemoveEmployeeByFirstName(string firstName)
+        {
+            Employee employee_to_remove = FindEmployeeByFirstName(firstName);
+            if(employee_to_remove !=null)
+            {
+                Context.Employee.Remove(employee_to_remove);
+                Context.SaveChanges();
+            }
+            return employee_to_remove;
+        }
+
+        public void AddNewHabitat(HabitatType habitat)
+        {
+            Context.Habitat.Add(habitat);
+            Context.SaveChanges();
+        }
+        public HabitatType FindHabitatByName(string habitatName)
+        {
+            HabitatType found_habitat = Context.Habitat.FirstOrDefault(e => e.Habitat.ToLower() == habitatName.ToLower());
+            return found_habitat;
+        }
+        public HabitatType RemoveHabitatByType(string habitatType)
+        {
+            HabitatType habitat_to_remove = FindHabitatByName(habitatType);
+            if (habitatType != null)
+            {
+                Context.Habitat.Remove(habitat_to_remove);
+                Context.SaveChanges();
+            }
+            return habitat_to_remove;
+        }
+
+        public void AddNewSpecies(Species species)
+        {
+            Context.Species.Add(species);
+            Context.SaveChanges();
+        }
+        public Species FindSpeciesByCommonName(string commonName)
+        {
+            Species found_species = Context.Species.FirstOrDefault(e => e.Common_Name.ToLower() == commonName.ToLower());
+            return found_species;
+        }
+        public Species RemoveSpeciesByCommonName(string commonName)
+        {
+            Species species_to_remove = FindSpeciesByCommonName(commonName);
+            if (commonName != null)
+            {
+                Context.Species.Remove(species_to_remove);
+                Context.SaveChanges();
+            }
+            return species_to_remove;
+        }
+
+        public Exhibit FindExhibitByName(string exhibitName)
+        {
+            Exhibit found_exhibit = Context.Exhibit.FirstOrDefault(e => e.Name.ToLower() == exhibitName.ToLower());
+            return found_exhibit;
+        }
+        public Exhibit RemoveExhibitByName(string exhibitName)
+        {
+            Exhibit exhibit_to_remove = FindExhibitByName(exhibitName);
+            if (exhibitName != null)
+            {
+                Context.Exhibit.Remove(exhibit_to_remove);
+                Context.SaveChanges();
+            }
+            return exhibit_to_remove;
+        }
+
+
 
     }
 }
