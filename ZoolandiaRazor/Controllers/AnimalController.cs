5a8a3aac661df63e8bb125a3f84e1274f2f8663d
Dan Ventura <danwventura@icloud.com>
2016-10-10 19:13:21 -0500

    animal-controller added GetAllAnimals and GetAnimalById methods

--- ZoolandiaRazor/Controllers/AnimalController.cs
+++ ZoolandiaRazor/Controllers/AnimalController.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ZoolandiaRazor.DAL;
 
 namespace ZoolandiaRazor.Controllers
 {
     public class AnimalController : Controller
     {
+        ZoolandiaRazorRepo repo = new ZoolandiaRazorRepo();
         // GET: Animal
         public ActionResult Index()
         {
+            ViewBag.All_Animals = repo.GetAllAnimals();
             return View();
         }
 
         // GET: Animal/Details/5
         public ActionResult Details(int id)
         {
+            ViewBag.This_Animal = repo.FindAnimalById(id);
             return View();
         }
 
